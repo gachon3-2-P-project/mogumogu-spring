7b2fb899aa53e09657b1210fb39d14de4300063b
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2023-12-10 09:21:17 +0900

    🌱yml 파일 gitignore 등록

--- article/src/test/java/com/mogumogu/spring/ArticleApplicationTests.java
+++ article/src/test/java/com/mogumogu/spring/ArticleApplicationTests.java
@@ -1,5 +1,6 @@
-package com.mogumogu.article;
+package com.mogumogu.spring;
 
+import com.mogumogu.spring.repository.ArticleRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
