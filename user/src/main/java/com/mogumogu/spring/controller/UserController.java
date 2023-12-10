419addb84d6a4ccf0be7b2bcd978abdac33353e1
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2023-12-11 07:42:01 +0900

    ✨User domain 기능 추가

--- user/src/main/java/com/mogumogu/spring/controller/UserController.java
+++ user/src/main/java/com/mogumogu/spring/controller/UserController.java
@@ -1,19 +1,63 @@
 package com.mogumogu.spring.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mogumogu.spring.dto.UserDto;
+import com.mogumogu.spring.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class UserController {
+
+  private final UserService userService;
 
   @GetMapping("/test")
   public String test() {
     log.info("User - Test()");
     return "User - Test()";
+  }
+
+  /**
+   * 유저 고유 id로 조회
+   */
+  @GetMapping("/get")
+  public ResponseEntity<?> getUser (@RequestParam("userId") Long userId) {
+
+    UserDto.UserResponseDto user = userService.getUser(userId);
+    return ResponseEntity.ok().body(user);
+  }
+
+  /**
+   *
+   * update -> 닉네임 수정
+   */
+  @PatchMapping("/update")
+  public ResponseEntity<?> updateUser(@RequestParam("userId") Long userId, @RequestBody UserDto.UserPatchDto userPatchDto) {
+    return ResponseEntity.ok().body(userService.updateUser(userId, userPatchDto));
+  }
+
+  /**
+   * 사용자 탈퇴 기능
+   */
+  @DeleteMapping("/delete")
+  public ResponseEntity<?> deleteUser(@RequestParam("userId") long userId) {
+    userService.deleteUser(userId);
+    return ResponseEntity.ok().body("Deleted UserId: " + userId);
+  }
+
+  /**
+   * 관리자 사용자 전체 조회
+   */
+  @GetMapping("/admin/getAll")
+  public ResponseEntity<List<UserDto.UserResponseDto>> getAllUser() {
+    List<UserDto.UserResponseDto> users = userService.getAllUser();
+    return ResponseEntity.ok().body(users);
   }
 
 }
