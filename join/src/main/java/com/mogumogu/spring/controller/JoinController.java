b72950b5b5e0c73e05cfc429089408b486ebe788
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2023-12-11 16:45:14 +0900

    🐛️회원가입 엔드포인트 수정

--- join/src/main/java/com/mogumogu/spring/controller/JoinController.java
+++ join/src/main/java/com/mogumogu/spring/controller/JoinController.java
@@ -47,7 +47,7 @@ public class JoinController {
     /**
      * 회원 가입
      */
-    @PostMapping(value = "/join")
+    @PostMapping(value = "/process")
     public ResponseEntity<?> createUser(@RequestBody UserDto.UserRequestDto userRequestDto) {
         //로그
 
