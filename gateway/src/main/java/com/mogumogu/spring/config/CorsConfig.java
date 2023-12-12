4b2729a5fb530e66f91cd848b03e153b1da503ed
githyuniiee <https://github.com/githyuniiee>
2023-12-12 18:07:48 +0900

    cors 수정

--- gateway/src/main/java/com/mogumogu/spring/config/CorsConfig.java
+++ gateway/src/main/java/com/mogumogu/spring/config/CorsConfig.java
@@ -1,25 +1,48 @@
 package com.mogumogu.spring.config;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
-public class CorsConfig {
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("*")); //
-        config.addAllowedHeader("*");
-        config.setAllowedMethods(List.of("GET","POST","PATCH","DELETE"));
+public class CorsConfig implements WebMvcConfigurer {
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+//        config.setAllowedOrigins(List.of("*")); //
+//        config.addAllowedHeader("*");
+//        config.setAllowedMethods(List.of("GET","POST","PATCH","DELETE"));
+//
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsFilter(source);
+//    }
 
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .maxAge(3000)
+                .allowedHeaders("*")
+                .exposedHeaders(HttpHeaders.LOCATION);
+    }
+
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new LoggerInterceptor())
+//                .excludePathPatterns("/css/**", "/images/**", "/js/**");
+
+//        registry.addInterceptor(new LoginCheckInterceptor())
+//                //.addPathPatterns("/*")
+//                .excludePathPatterns("/admin/log*");
     }
 }
