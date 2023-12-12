9a7e7b98f5ac4b18b9b6a1db3a30580cb0a8eb0b
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2023-12-12 11:40:26 +0900

    🔖운영 서버 버전 생성

--- gateway/src/main/java/com/mogumogu/spring/GatewayApplication.java
+++ gateway/src/main/java/com/mogumogu/spring/GatewayApplication.java
@@ -3,6 +3,9 @@ package com.mogumogu.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
@@ -13,4 +16,5 @@ public class GatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
+
 }
