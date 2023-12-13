216c2f49b7f3b925d4e6b3898be7a7cd59dc57be
githyuniiee <https://github.com/githyuniiee>
2023-12-14 01:34:40 +0900

    toekn body 로

--- user/src/main/java/com/mogumogu/spring/security/JwtAuthenticationFilter.java
+++ user/src/main/java/com/mogumogu/spring/security/JwtAuthenticationFilter.java
@@ -78,10 +78,11 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .withClaim("username", principalDetailis.getPerson().getUsername())
                 .sign(Algorithm.HMAC512(JwtProperties.secret));
 
-        response.addHeader(JwtProperties.headerString, jwtToken);
+        //response.addHeader(JwtProperties.headerString, jwtToken);
 
         // 바디에도 토큰 정보 추가
         Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("token " + JwtProperties.headerString, jwtToken);
         responseBody.put("username", principalDetailis.getPerson().getUsername());
         responseBody.put("id", principalDetailis.getPerson().getId());
 
