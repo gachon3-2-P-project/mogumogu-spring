04d05d691ac309649fc1dab6bec6a79d18fcc58c
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2023-12-13 12:33:43 +0900

    💡Join init 주석 처리

--- join/src/main/java/com/mogumogu/spring/JoinInit.java
+++ join/src/main/java/com/mogumogu/spring/JoinInit.java
@@ -13,61 +13,61 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
-@RequiredArgsConstructor
-@Slf4j
-public class JoinInit {
-
-    private final UserRepository userRepository;
-    private final AdminRepository adminRepository;
-    private final ArticleRepository articleRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-
-    @PostConstruct
-    private void initFirst(){
-        initAdmins();
-        initUsers();
-        initArticles();
-    }
-
-    @Transactional
-    public void initAdmins() {
-
-        AdminEntity admin = new AdminEntity();
-        admin.setUsername("admin_0"); //관리자아이디
-        admin.setPassword(bCryptPasswordEncoder.encode("admin_" + 0));
-        admin.setRole(Role.ADMIN);
-        adminRepository.save(admin);
-
-
-    }
-
-    @Transactional
-    public void initUsers() {
-        for (int i = 0; i < 5; i++) {
-            UserEntity user = new UserEntity();
-            user.setUsername("gachon" + (i+1) + "@gachon.ac.kr");
-            user.setNickName("userNickname" + (i+1));
-            user.setPassword(bCryptPasswordEncoder.encode("member" + i));
-            user.setRole(Role.USER);
-            userRepository.save(user);
-        }
-    }
-
-    @Transactional
-    public void initArticles() {
-        List<UserEntity> user = userRepository.findAll();
-        for (int i = 0; i < 5; i++) {
-            ArticleEntity article = new ArticleEntity();
-            article.setUser(user.get(i));
-            article.setTitle("제목" + i);
-            article.setContent("Content");
-            article.setNumberOfPeople(3);
-            article.setCost(10000 * i);
-            article.setComplain(0);
-            articleRepository.save(article);
-        }
-    }
-
-
-}
+//@Component
+//@RequiredArgsConstructor
+//@Slf4j
+//public class JoinInit {
+//
+//    private final UserRepository userRepository;
+//    private final AdminRepository adminRepository;
+//    private final ArticleRepository articleRepository;
+//    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+//
+//    @PostConstruct
+//    private void initFirst(){
+//        initAdmins();
+//        initUsers();
+//        initArticles();
+//    }
+//
+//    @Transactional
+//    public void initAdmins() {
+//
+//        AdminEntity admin = new AdminEntity();
+//        admin.setUsername("admin_0"); //관리자아이디
+//        admin.setPassword(bCryptPasswordEncoder.encode("admin_" + 0));
+//        admin.setRole(Role.ADMIN);
+//        adminRepository.save(admin);
+//
+//
+//    }
+//
+//    @Transactional
+//    public void initUsers() {
+//        for (int i = 0; i < 5; i++) {
+//            UserEntity user = new UserEntity();
+//            user.setUsername("gachon" + (i+1) + "@gachon.ac.kr");
+//            user.setNickName("userNickname" + (i+1));
+//            user.setPassword(bCryptPasswordEncoder.encode("member" + i));
+//            user.setRole(Role.USER);
+//            userRepository.save(user);
+//        }
+//    }
+//
+//    @Transactional
+//    public void initArticles() {
+//        List<UserEntity> user = userRepository.findAll();
+//        for (int i = 0; i < 5; i++) {
+//            ArticleEntity article = new ArticleEntity();
+//            article.setUser(user.get(i));
+//            article.setTitle("제목" + i);
+//            article.setContent("Content");
+//            article.setNumberOfPeople(3);
+//            article.setCost(10000 * i);
+//            article.setComplain(0);
+//            articleRepository.save(article);
+//        }
+//    }
+//
+//
+//}
