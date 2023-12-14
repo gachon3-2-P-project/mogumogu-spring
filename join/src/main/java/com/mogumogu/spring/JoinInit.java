dd0671a863c5389768a3d0edfc2a4d88c53293f9
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2023-12-15 08:13:26 +0900

    🗃 DB 리셋

--- join/src/main/java/com/mogumogu/spring/JoinInit.java
+++ join/src/main/java/com/mogumogu/spring/JoinInit.java
@@ -13,32 +13,30 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-//@Component
-//@RequiredArgsConstructor
-//@Slf4j
-//public class JoinInit {
-//    private final UserRepository userRepository;
-//    private final AdminRepository adminRepository;
-//    private final ArticleRepository articleRepository;
-//    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-//
-//    @PostConstruct
-//    private void initFirst(){
-//        initAdmins();
-//        initUsers();
-//        initArticles();
-//    }
-//
-//    @Transactional
-//    public void initAdmins() {
-//        AdminEntity admin = new AdminEntity();
-//        admin.setUsername("admin_0"); //관리자아이디
-//        admin.setPassword(bCryptPasswordEncoder.encode("admin_" + 0));
-//        admin.setRole(Role.ADMIN);
-//        adminRepository.save(admin);
-//    }
-//
-//
-//
-//
-//}
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class JoinInit {
+    private final UserRepository userRepository;
+    private final AdminRepository adminRepository;
+    private final ArticleRepository articleRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @PostConstruct
+    private void initFirst(){
+        initAdmins();
+    }
+
+    @Transactional
+    public void initAdmins() {
+        AdminEntity admin = new AdminEntity();
+        admin.setUsername("admin_0"); //관리자아이디
+        admin.setPassword(bCryptPasswordEncoder.encode("admin_" + 0));
+        admin.setRole(Role.ADMIN);
+        adminRepository.save(admin);
+    }
+
+
+
+
+}
