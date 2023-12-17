a11dc8c82f9a53f7401658adc68d376c8dede813
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2023-12-18 02:54:51 +0900

    ♻️메세지 id값으로 재정렬

--- message/src/main/java/com/mogumogu/spring/service/MessageService.java
+++ message/src/main/java/com/mogumogu/spring/service/MessageService.java
@@ -17,10 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -176,6 +173,8 @@ public class MessageService {
         List<MessageEntity> allMessages = new ArrayList<>();
         allMessages.addAll(authorMessages);
         allMessages.addAll(userMessages);
+
+        allMessages.sort(Comparator.comparingLong(MessageEntity::getId));
 
         log.info("Number of All Messages: {}", allMessages.size());
 
