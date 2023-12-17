45effbc3cd4fc05dd72e10dfd538ce08e3f9f583
githyuniiee <https://github.com/githyuniiee>
2023-12-18 03:00:29 +0900

    id 재정렬

--- message/src/main/java/com/mogumogu/spring/service/MessageService.java
+++ message/src/main/java/com/mogumogu/spring/service/MessageService.java
@@ -17,10 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -193,6 +190,7 @@ public class MessageService {
 
                     return messageResponseDto;
                 })
+                .sorted(Comparator.comparingLong(MessageDto.MessageResponseDto::getId))
                 .collect(Collectors.toList());
 
         return messageDtos;
